b220b9892b80682bc5404c1380a1d4c25681f02f
Jhe <jeremiahpasion56@gmail.com>
2024-05-28 22:42:33 +0800

    init backend

---
+++ backend/src/main/java/com/example/RealTime_TicTacToe/RealTimeTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealTime_TicTacToe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTicTacToeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/RealTime_TicTacToe/RealTimeTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealTime_TicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
