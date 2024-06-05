2f8ac20d800a150835f2e294cc06583ba01c253d
Jhe <jeremiahpasion56@gmail.com>
2024-06-05 23:49:44 +0800

    add PlayerDto

---
+++ backend/src/main/java/com/example/TikTacToe/dto/PlayerDto.java
@@ -0,0 +1,13 @@
+package com.example.TikTacToe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlayerDto {
+
+    private String playerId;
+
+    private String symbol;
+}
