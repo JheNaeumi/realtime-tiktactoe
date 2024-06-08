ac63e0ebb58356b622db7c94a57c2b0af02dace1
Jhe <jeremiahpasion56@gmail.com>
2024-06-08 22:13:36 +0800

    update websocketconfig

--- backend/src/main/java/com/example/TikTacToe/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/TikTacToe/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/tictactoe-websocket").setAllowedOrigins("http://localhost:5173").withSockJS();
+        registry.addEndpoint("/tictactoe").setAllowedOrigins("http://localhost:5173").withSockJS();
     }
 }
 
