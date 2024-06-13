e430e8361515191d38dfb2549218132ef5062ba3
Jhe <jeremiahpasion56@gmail.com>
2024-06-13 23:33:50 +0800

    update successHandler

--- backend/src/main/java/com/example/TikTacToe/config/OAuthSuccessHandler.java
+++ backend/src/main/java/com/example/TikTacToe/config/OAuthSuccessHandler.java
@@ -1,26 +1,62 @@
 package com.example.TikTacToe.config;
 
+import com.example.TikTacToe.entity.Role;
+import com.example.TikTacToe.entity.Source;
+import com.example.TikTacToe.entity.User;
+import com.example.TikTacToe.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class OAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
 
-
+    private final UserService userService;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+        if ("google".equals(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId())) {
+            DefaultOAuth2User principal = (DefaultOAuth2User) authentication.getPrincipal();
+            Map<String, Object> attributes = principal.getAttributes();
+            String email = attributes.getOrDefault("email", "").toString();
+            String name = attributes.getOrDefault("name", "").toString();
+            userService.findByEmail(email)
+                    .ifPresentOrElse(user -> {
+                        DefaultOAuth2User newUser = new DefaultOAuth2User(List.of(new SimpleGrantedAuthority(user.getRole().name())),
+                                attributes, "sub");
+                        Authentication securityAuth = new OAuth2AuthenticationToken(newUser, List.of(new SimpleGrantedAuthority(user.getRole().name())),
+                                oAuth2AuthenticationToken.getAuthorizedClientRegistrationId());
+                        SecurityContextHolder.getContext().setAuthentication(securityAuth);
+                    }, () -> {
+                        User userEntity = new User();
+                        userEntity.setRole(Role.ROLE_USER);
+                        userEntity.setEmail(email);
+                        userEntity.setName(name);
+                        userEntity.setSource(Source.GOOGLE);
+                        userService.save(userEntity);
+                        DefaultOAuth2User newUser = new DefaultOAuth2User(List.of(new SimpleGrantedAuthority(userEntity.getRole().name())),
+                                attributes, "sub");
+                        Authentication securityAuth = new OAuth2AuthenticationToken(newUser, List.of(new SimpleGrantedAuthority(userEntity.getRole().name())),
+                                oAuth2AuthenticationToken.getAuthorizedClientRegistrationId());
+                        SecurityContextHolder.getContext().setAuthentication(securityAuth);
+                    });
+        }
         this.setAlwaysUseDefaultTargetUrl(true);
-        this.setDefaultTargetUrl("http://localhost:5173/");
+        this.setDefaultTargetUrl("http://localhost:5173");
         super.onAuthenticationSuccess(request, response, authentication);
     }
 }
