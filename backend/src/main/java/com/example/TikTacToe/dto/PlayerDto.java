ba0103e649f7aa97a23ecb27d2a3e9447020306d
Jhe <jeremiahpasion56@gmail.com>
2024-06-16 22:18:48 +0800

    update add gameId

--- backend/src/main/java/com/example/TikTacToe/dto/PlayerDto.java
+++ backend/src/main/java/com/example/TikTacToe/dto/PlayerDto.java
@@ -6,8 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PlayerDto {
-
     private String playerId;
-
     private String symbol;
+    private String gameId; // Add this field to include the game ID
 }
