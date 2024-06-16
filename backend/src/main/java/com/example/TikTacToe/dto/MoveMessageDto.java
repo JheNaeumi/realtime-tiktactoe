a3114349a3f33f1ea4963f55fb8105400cd1d85b
Jhe <jeremiahpasion56@gmail.com>
2024-06-16 22:19:01 +0800

    update add gameId & playerId

--- backend/src/main/java/com/example/TikTacToe/dto/MoveMessageDto.java
+++ backend/src/main/java/com/example/TikTacToe/dto/MoveMessageDto.java
@@ -7,10 +7,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MoveMessageDto {
+    private String gameId;
+    private String playerId;
     private int row;
     private int col;
     private char from;
-    private String playerId;
-
 
 }
