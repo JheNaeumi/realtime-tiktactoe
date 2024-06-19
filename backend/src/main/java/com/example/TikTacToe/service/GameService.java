bf25e19aec3b924b945c5d52a942c963c8a36998
Jhe <jeremiahpasion56@gmail.com>
2024-06-19 23:36:19 +0800

    add GameService_Interface

---
+++ backend/src/main/java/com/example/TikTacToe/service/GameService.java
@@ -0,0 +1,15 @@
+package com.example.TikTacToe.service;
+
+import com.example.TikTacToe.entity.Game;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GameService {
+
+    Game getAvailableGame();
+
+    Optional<Game> getGameById(String gameId);
+
+    List<Game> getGames();
+}
