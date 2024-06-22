b0304fc408da811d1b6d3025038ad197809a46ad
Jhe <jeremiahpasion56@gmail.com>
2024-06-22 23:18:35 +0800

    add error component route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,17 @@
 import React, { useState } from "react";
 import Game from "./component/Game";
+import Error from "./component/Error";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import { loggedIn } from "./service/api";
+
 
 function App() {
-  const [isAuth, setAuth] = useState(false);
-  useEffect(()=>{
-    try {
-      const response=  loggedIn();
-      if(response.status === 200){
-        setAuth(true)
-        console.log(isAuth)
-      }else{
-        console.log(isAuth)
-      }
-    } catch (error) {
-      
-    }
-  })
+
   return(
     <>
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Game/> && isAuth }></Route>
+            <Route path="/" element={<Game/>}></Route>
+            <Route path="/error" element={<Error/>}></Route>
         </Routes>
     </BrowserRouter>
     </>
