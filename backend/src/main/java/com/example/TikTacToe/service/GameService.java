1ca31050d8b007adf28a353d24c9d57bcb11ce1f
Jhe <jeremiahpasion56@gmail.com>
2024-06-28 22:51:42 +0800

    update gameService

--- backend/src/main/java/com/example/TikTacToe/service/GameService.java
+++ backend/src/main/java/com/example/TikTacToe/service/GameService.java
@@ -3,13 +3,12 @@ package com.example.TikTacToe.service;
 import com.example.TikTacToe.entity.Game;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface GameService {
 
     Game getAvailableGame();
 
-    Optional<Game> getGameById(String gameId);
+    Game getGameById(String gameId);
 
     List<Game> getGames();
 
