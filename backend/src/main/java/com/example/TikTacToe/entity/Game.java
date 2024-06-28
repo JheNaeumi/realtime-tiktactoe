e4f8a06b7e16d0ae8144b96c7d46dbda68f1629c
Jhe <jeremiahpasion56@gmail.com>
2024-06-28 22:51:28 +0800

    update game_constructor & add isCompleted

--- backend/src/main/java/com/example/TikTacToe/entity/Game.java
+++ backend/src/main/java/com/example/TikTacToe/entity/Game.java
@@ -1,6 +1,7 @@
 package com.example.TikTacToe.entity;
 
-import com.example.TikTacToe.dto.GameStateDto;
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,18 +9,33 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String gameId;
     private String playerX;
     private String playerO;
-    private GameStateDto gameState;
 
+
+    @OneToOne(   cascade = CascadeType.ALL)
+    @JoinColumn(name = "game_state_id", referencedColumnName = "id")
+    private GameState gameState;
     public Game() {
         this.gameId = UUID.randomUUID().toString();
-        this.gameState = new GameStateDto();
+        this.gameState = new GameState();
     }
 
     public boolean isFull() {
         return playerX != null && playerO != null;
+    }
+
+    public boolean isCompleted() {
+        return "X wins!".equals(gameState.getStatusMessage()) ||
+                "O wins!".equals(gameState.getStatusMessage()) ||
+                "Draw!".equals(gameState.getStatusMessage());
     }
 }
