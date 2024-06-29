c254468809586954eb6bde1261591320befb5cf3
Jhe <jeremiahpasion56@gmail.com>
2024-06-29 23:26:48 +0800

    add Allargs Annotation

--- backend/src/main/java/com/example/TikTacToe/dto/GameStateDto.java
+++ backend/src/main/java/com/example/TikTacToe/dto/GameStateDto.java
@@ -1,25 +1,16 @@
 package com.example.TikTacToe.dto;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class GameStateDto {
     private char[][] board;
     private char currentPlayer;
     private String statusMessage;
 
-    public GameStateDto() {
-        this.board = new char[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '\0';
-            }
-        }
-        this.currentPlayer = 'X';
-        this.statusMessage = "Game in progress";
-    }
-
 }
+
