d8b300141f7e41d9acecec895ab82d84f44a9200
Jhe <jeremiahpasion56@gmail.com>
2024-06-30 22:31:08 +0800

    update toGameStateDto mapper

--- backend/src/main/java/com/example/TikTacToe/Mapper/GameStateMapper.java
+++ backend/src/main/java/com/example/TikTacToe/Mapper/GameStateMapper.java
@@ -7,5 +7,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface GameStateMapper {
 
-    GameStateDto toGameStateDto(GameState gameState);
+    default GameStateDto toGameStateDto(GameState gameState) {
+        return new GameStateDto(gameState.getBoardArray(), gameState.getCurrentPlayer(), gameState.getStatusMessage());
+    }
 }
